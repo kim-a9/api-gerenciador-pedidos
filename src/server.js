7c58bbbf2654e0ce1adc70e06c08b960995090ae
kim-a9 <kimberlysantosalves@gmail.com>
2026-03-07 22:03:42 -0300

    feat: implementação da rota post, server

--- src/server.js
+++ src/server.js
@@ -1,27 +1,32 @@
+require('reflect-metadata');
 require('dotenv').config();
-// const app = require('./app');
+const ApiDataSource = require('./database/databaseConfig');
 const express = require('express');
-const routes = require('./routes/Routes');
+const router = require('./routes/Routes');
 
 
 const app = express();
 
 app.use(express.json());
-app.use(routes);
-
-
-const PORT = process.env.PORT;
-
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({
-    message: 'Ocorreu um erro interno no servidor',
-    error: process.env.NODE_ENV === 'development' ? err.message : {}
-  });
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+        return res.status(400).json({ message: 'JSON inválido no corpo da requisição' });
+    }
+    next();
 });
+app.use(router);
 
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em: http://localhost:${PORT}`);
-});
+ApiDataSource.initialize()
+  .then(() => {
+    console.log('Conexão com Banco de Dados realizada com sucesso!');
+
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando em: http://localhost:${PORT}`);
+    });
+    
+  }).catch((error) => console.log("Erro na conexão ao banco de dados", error))
+
+
